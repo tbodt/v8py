9d46bd184c61d9a86f07434348c18e8f4ae71ce4
Theodore Dubois <tblodt@icloud.com>
2016-11-09 16:03:37 -0800

    Move the v8 building process into a script

---
+++ build-v8.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ -d v8 ]; then
+    echo 'v8 appears to already have been built'
+    exit
+fi
+
+# install depot_tools
+# honestly, fuck google and their idiot build system
+if [ ! -d depot_tools ]; then
+    git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+fi
+export PATH=`pwd`/depot_tools:$PATH
+
+# build v8
+fetch v8
+cd v8
+git checkout branch-heads/5.4
+gclient sync
+make native -j2
