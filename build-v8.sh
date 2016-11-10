d9cff005a264895b2cc4357915b4f812dd0bab07
Theodore Dubois <tblodt@icloud.com>
2016-11-09 16:17:25 -0800

    Largish improvements to v8 build script

--- build-v8.sh
+++ build-v8.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
+
+function run() {
+    echo "$ $@"
+    $@
+    if [ $? -ne 0 ]; then
+        exit
+    fi
+}
+
 if [ -d v8/.git ]; then
     echo 'v8 appears to already have been built'
     exit
+else
+    rm -rf v8
 fi
 
 # install depot_tools
 # honestly, fuck google and their idiot build system
 if [ ! -d depot_tools ]; then
-    git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+    run git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 fi
 export PATH=`pwd`/depot_tools:$PATH
 
 # build v8
-fetch v8
-cd v8
-git checkout branch-heads/5.4
-gclient sync
-make native -j2
+run fetch v8
+run cd v8
+run git checkout branch-heads/5.4
+run gclient sync
+run make native -j2
