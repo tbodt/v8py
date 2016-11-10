a13dca043e250fd4c0e40c5a136c6f6feb4b1a31
Theodore Dubois <tblodt@icloud.com>
2016-11-10 09:29:44 -0800

    Try and use the correct compiler for building v8

--- build-v8.sh
+++ build-v8.sh
@@ -15,12 +15,16 @@ else
     rm -rf v8
 fi
 
+# google wants to use their own compiler, and I don't want to stop them
+run unset CC
+run unset CXX
+
 # install depot_tools
 # honestly, fuck google and their idiot build system
 if [ ! -d depot_tools ]; then
     run git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 fi
-export PATH=`pwd`/depot_tools:$PATH
+run export PATH=`pwd`/depot_tools:$PATH
 
 # build v8
 run fetch v8
@@ -29,6 +33,6 @@ run git checkout branch-heads/5.4
 run gclient sync
 # v8 uses clang warning flags that are too bleeding edge
 # this would be ok, but they also use -Werror
-run sed -i.bak /no-undefined-var-template/d gypfiles/standalone.gypi
-run sed -i.bak /no-nonportable-include-path/d gypfiles/standalone.gypi
+# run sed -i.bak /no-undefined-var-template/d gypfiles/standalone.gypi
+# run sed -i.bak /no-nonportable-include-path/d gypfiles/standalone.gypi
 run make native -j2
