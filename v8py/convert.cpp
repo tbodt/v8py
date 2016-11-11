ab4eb2a493b5bc873c701cc1baf57dfd10d6988a
Theodore Dubois <tblodt@icloud.com>
2016-11-10 18:15:10 -0800

    Prevent v8 from overrunning the buffer by writing a null terminator

--- v8py/convert.cpp
+++ v8py/convert.cpp
@@ -45,7 +45,7 @@ PyObject *py_from_js(Local<Value> value, Local<Context> context) {
         Local<String> str_value = value.As<String>();
         size_t bufsize = str_value->Length() * sizeof(uint16_t);
         uint16_t *buf = (uint16_t *) malloc(bufsize);
-        str_value->Write(buf, 0, bufsize);
+        str_value->Write(buf, 0, bufsize, String::WriteOptions::NO_NULL_TERMINATION);
         PyObject *py_value = PyUnicode_DecodeUTF16((const char *) buf, bufsize, NULL, NULL);
         free(buf);
         return py_value;
