f2f2740e4f7168bebd733394aaa0947906d85222
Theodore Dubois <tblodt@icloud.com>
2016-11-11 20:46:38 -0800

    Put libicui18n before libicuuc

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
                                 'v8/out/native/obj.target/third_party/icu'],
                   libraries=['v8_libplatform', 'v8_base', 'v8_nosnapshot',
                              'v8_libbase', 'v8_libsampler',
-                             'icuuc', 'icui18n'],
+                             'icui18n', 'icuuc'],
                   extra_compile_args=['-std=c++11'],
                   ),
     ],
