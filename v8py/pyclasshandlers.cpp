c28d9b0450b20ae16c7203939d3fc32ca58c4370
Theodore Dubois <tblodt@icloud.com>
2016-11-15 17:09:02 -0800

    Add too many TODOs to pyclasshandlers

--- v8py/pyclasshandlers.cpp
+++ v8py/pyclasshandlers.cpp
@@ -18,6 +18,9 @@ void py_class_construct_callback(const FunctionCallbackInfo<Value> &info) {
 
     Local<Object> js_new_object = info.This();
     PyObject *new_object = PyObject_Call(self->cls, pys_from_jss(info, context), NULL);
+    if (new_object == NULL) {
+        // TODO implement exception handling for if new_object == NULL
+    }
     py_class_init_js_object(js_new_object, new_object);
 }
 
@@ -29,21 +32,37 @@ void py_class_method_callback(const FunctionCallbackInfo<Value> &info) {
     Local<Object> js_self = info.This();
     PyObject *self = (PyObject *) js_self->GetInternalField(1).As<External>()->Value();
     PyObject *args = pys_from_jss(info, context);
+    if (args == NULL) {
+        return;
+    }
 
     PyObject *method_name = py_from_js(info.Data(), context);
+    if (method_name == NULL) {
+        Py_DECREF(args);
+        return;
+    }
     PyObject *method = PyObject_GetAttr(self, method_name);
+    if (method == NULL) {
+        Py_DECREF(method_name);
+        Py_DECREF(args);
+        return;
+    }
     PyObject *retval = PyObject_Call(method, args, NULL);
+    if (retval == NULL) {
+        // TODO implement exception handling
+    }
 
     info.GetReturnValue().Set(js_from_py(retval, context));
 
-    Py_DECREF(method_name);
-    Py_DECREF(method);
-    Py_DECREF(args);
     Py_DECREF(retval);
+    Py_DECREF(method);
+    Py_DECREF(method_name);
+    Py_DECREF(args);
 }
 
 // --- Interceptors ---
 // TODO everything needs to be fixed with regards to exception handling
+// one TODO comment for everything that needs to be fixed
 
 template <class T> PyObject *get_self(const PropertyCallbackInfo<T> &info) {
     return (PyObject *) info.This()->GetInternalField(1).template As<External>()->Value();
@@ -55,8 +74,14 @@ void py_class_getter_callback(Local<Name> js_name, const PropertyCallbackInfo<Va
     Local<Context> context = isolate->GetCurrentContext();
     
     PyObject *name = py_from_js(js_name, context);
+    if (name == NULL) {
+        // TODO
+        return;
+    }
     PyObject *value = PyObject_GetItem(get_self(info), name);
-    if (value != NULL) {
+    if (value == NULL) {
+        // TODO
+    } else {
         info.GetReturnValue().Set(js_from_py(value, context));
         Py_DECREF(value);
     }
@@ -69,8 +94,22 @@ void py_class_setter_callback(Local<Name> js_name, Local<Value> js_value, const 
     Local<Context> context = isolate->GetCurrentContext();
 
     PyObject *name = py_from_js(js_name, context);
+    if (name == NULL) {
+        // TODO
+        return;
+    }
     PyObject *value = py_from_js(js_value, context);
-    if (PyObject_SetItem(get_self(info), name, value) >= 0) {
+    if (value == NULL) {
+        // TODO
+        Py_DECREF(name);
+        return;
+    }
+    if (PyObject_SetItem(get_self(info), name, value) < 0) {
+        // TODO
+        Py_DECREF(value);
+        Py_DECREF(name);
+        return;
+    } else {
         info.GetReturnValue().Set(js_value);
     }
     Py_DECREF(name);
@@ -98,10 +137,17 @@ void py_class_deleter_callback(Local<Name> js_name, const PropertyCallbackInfo<B
     Local<Context> context = isolate->GetCurrentContext();
 
     PyObject *name = py_from_js(js_name, context);
-    if (PyObject_DelItem(get_self(info), name) >= 0) {
-        info.GetReturnValue().Set(True(isolate));
-    } else {
+    if (name == NULL) {
+        // TODO
+        return;
+    }
+    if (PyObject_DelItem(get_self(info), name) < 0) {
+        if (info.ShouldThrowOnError()) {
+            // TODO
+        }
         info.GetReturnValue().Set(False(isolate));
+    } else {
+        info.GetReturnValue().Set(True(isolate));
     }
     Py_DECREF(name);
 }
@@ -112,9 +158,19 @@ void py_class_enumerator_callback(const PropertyCallbackInfo<Array> &info) {
     Local<Context> context = isolate->GetCurrentContext();
 
     PyObject *keys = PyObject_CallMethod(get_self(info), "keys", "");
+    if (keys == NULL) {
+        // TODO
+        return;
+    }
     Local<Array> js_keys = Array::New(isolate, PySequence_Length(keys));
     for (int i = 0; i < PySequence_Length(keys); i++) {
-        js_keys->Set(context, i, js_from_py(PySequence_ITEM(keys, i), context)).FromJust();
+        PyObject *item = PySequence_ITEM(keys, i);
+        if (item == NULL) {
+            // TODO
+            Py_DECREF(keys);
+            return;
+        }
+        js_keys->Set(context, i, js_from_py(item, context)).FromJust();
     }
     info.GetReturnValue().Set(js_keys);
 }
