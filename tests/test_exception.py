8dbe8f759181254d3622b6b5ff9fb87bc3943df9
Theodore Dubois <tblodt@icloud.com>
2016-11-21 19:04:43 -0800

    Add exception handling tests

---
+++ tests/test_exception.py
@@ -0,0 +1,34 @@
+import pytest
+from v8py import JSException
+
+class CustomError(Exception):
+    def method(self):
+        return 'method'
+
+@pytest.fixture(params=[ValueError, CustomError])
+def ErrorClass(request):
+    return request.param
+
+@pytest.fixture
+def context(context, ErrorClass):
+    context.TestError = ErrorClass
+    def throw_exception():
+        raise ErrorClass
+    context.throw_exception = throw_exception
+    return context
+
+def test_javascript_to_python(context, ErrorClass):
+    with pytest.raises(JSException):
+        context.eval('throw new Error()')
+
+def test_python_to_javascript(context, ErrorClass):
+    context.eval("""
+try {
+    throw_exception();
+} catch (exc) {
+    this.exc = exc;
+}
+    """)
+    print ErrorClass
+    assert isinstance(context.exc, ErrorClass)
+    assert context.eval('exc instanceof TestError')
