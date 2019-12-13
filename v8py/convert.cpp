f5b3a69e0d059f4cc00ee1fb707700242dbc858b
Theodore Dubois <tblodt@icloud.com>
2019-12-12 20:27:28 -0800

    Unbreak build
    Lord forgive me, for I have sinned

--- v8py/convert.cpp
+++ v8py/convert.cpp
@@ -201,8 +201,9 @@ Local<Value> js_from_py(PyObject *value, Local<Context> context) {
     }
 
     if (PyList_Check(value) || PyTuple_Check(value)) {
+        int length = PySequence_Length(value);
         Local<Array> array = Array::New(isolate, length);
-        for (int i = 0; i < PySequence_Length(value); i++) {
+        for (int i = 0; i < length; i++) {
             PyObject *item = PySequence_ITEM(value, i);
             bool set_worked = array->Set(context, i, js_from_py(item, context)).FromJust();
             assert(set_worked);
